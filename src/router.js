e542c30bc0803a20e57a1a24a8f9f18e52becb85
maye-msft <34230764+maye-msft@users.noreply.github.com>
2018-06-25 21:57:27 +0800

    add cognitive service

--- src/router.js
+++ src/router.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var util = require('./util.js');
+
 router.get('/hello', function (req, res) {
     res.send(`hello ${new Date()}`)
 });
+
+router.post('/detectface', function (req, res) {
+    
+    
+    util.detectface(req.body,(response)=>{
+        res.json(response)
+    }, (error)=>{
+        res.send(JSON.parse(stringify(error)))
+    })
+
+});
+
 
 module.exports = router;
